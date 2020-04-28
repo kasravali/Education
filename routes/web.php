1cda81486bfd706797ea71391b4ab77aa57d6017
Hamed Movahed <hamed.movahed@gmail.com>
2020-04-29 00:19:37 +0430

    version hmm1:edit stcontroller and routes

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,4 @@ $router->get('/', function () use ($router) {
 $router->get('/student/list', 'StudentController@list');
 $router->post('/student/create', 'StudentController@create');
 $router->get('/student/read/{id}', 'StudentController@read');
-
+$router->get('/student/hi/{id}','StudentController@hello_student');
